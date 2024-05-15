565c9ad3395dce0c39133342dd19004a92227d1b
fade <dimabreusgo@gmail.com>
2024-05-15 21:02:06 +0300

    client created

---
+++ src/EchoClient/ReceiverThread.java
@@ -0,0 +1,39 @@
+package EchoClient;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+class ReceiverThread extends Thread {
+
+    private DatagramSocket socket;
+    private volatile boolean stopped = false;
+    ReceiverThread(DatagramSocket socket) {
+        this.socket = socket;
+    }
+    @Override
+    public void run() {
+        byte[] buffer = new byte[65507];
+        while (true) {
+            if (stopped)
+                return;
+            DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+            try {
+                try {
+                    socket.receive(dp);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                String s = new String(dp.getData(), 0, dp.getLength(), "UTF-8");
+                System.out.println(s);
+                Thread.yield();
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+        }
+    }
+
+    public void halt() {
+        this.stopped = true;
+    }
+}
